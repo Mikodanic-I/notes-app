36153d414570d29f7235dd0e5d1aecef1a6f0909
Ivan Mikodanic <ivan.mikodanic@gauss.hr>
2022-01-07 18:21:16 +0100

    refactor: rename static folder to assets

--- src/components/core/Icon.jsx
+++ src/components/core/Icon.jsx
@@ -1,10 +1,10 @@
 function Icon({ name }) {
     // TODO: find a better solution for this
     const icons = {
-        'icon-back': require(`../../static/icons/icon-back.svg`),
-        'icon-delete': require(`../../static/icons/icon-delete.svg`),
-        'icon-edit': require(`../../static/icons/icon-edit.svg`),
-        'icon-save': require(`../../static/icons/icon-save.svg`),
+        'icon-back': require(`../../assets/icons/icon-back.svg`),
+        'icon-delete': require(`../../assets/icons/icon-delete.svg`),
+        'icon-edit': require(`../../assets/icons/icon-edit.svg`),
+        'icon-save': require(`../../assets/icons/icon-save.svg`),
     }
 
     const { ReactComponent: IconSvg } = icons[name]
